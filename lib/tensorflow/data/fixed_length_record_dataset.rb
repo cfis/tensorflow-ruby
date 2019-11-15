671f3ea6cd378a5c3ba4ee87a1e4e6d30740fbd5
Charlie Savage <cfis@savagexi.com>
2019-11-15 08:28:01 -0800

    Fix input types.

--- lib/tensorflow/data/fixed_length_record_dataset.rb
+++ lib/tensorflow/data/fixed_length_record_dataset.rb
@@ -4,11 +4,17 @@ module Tensorflow
       def initialize(filenames, record_bytes, header_bytes: 0, footer_bytes: 0, buffer_size: DEFAULT_READER_BUFFER_SIZE_BYTES, compression_type: '')
         @output_types = [:string]
         @output_shapes = [[]]
+
+        record_bytes_tensor = Tensor.new(record_bytes, dtype: :int64)
+        header_bytes_tensor = Tensor.new(header_bytes, dtype: :int64)
+        footer_bytes_tensor = Tensor.new(footer_bytes, dtype: :int64)
+        buffer_size_tensor = Tensor.new(buffer_size, dtype: :int64)
+
         variant_tensor = RawOps.fixed_length_record_dataset_v2(filenames,
-                                                               header_bytes,
-                                                               record_bytes,
-                                                               footer_bytes,
-                                                               buffer_size,
+                                                               header_bytes_tensor,
+                                                               record_bytes_tensor,
+                                                               footer_bytes_tensor,
+                                                               buffer_size_tensor,
                                                                compression_type)
 
         super(variant_tensor)

997ac6b254db2c63af6741d9fe56c6c392e6fb8d
Charlie Savage <cfis@savagexi.com>
2019-11-18 16:59:25 -0800

    Get shuffle dataset working.

--- lib/tensorflow/data/shuffle_dataset.rb
+++ lib/tensorflow/data/shuffle_dataset.rb
@@ -2,11 +2,15 @@ module Tensorflow
   module Data
     class ShuffleDataset < Dataset
       def initialize(input_dataset, buffer_size)
-        @input_dataset = input_dataset # keep reference for memory
+        @input_dataset = input_dataset
         @output_types = input_dataset.output_types
         @output_shapes = input_dataset.output_shapes
 
-        variant_tensor = RawOps.shuffle_dataset(input_dataset,
+        buffer_size = Tensor.new(buffer_size, dtype: :int64)
+        seed = Tensor.new(::Random.rand(100_000_000), dtype: :int64)
+        seed2 = Tensor.new(::Random.rand(100_000_000), dtype: :int64)
+
+        variant_tensor = RawOps.shuffle_dataset(input_dataset.variant_tensor,
                                                 buffer_size,
                                                 seed,
                                                 seed2,
---
+++ test/data/shuffle_dataset_test.rb
@@ -0,0 +1,22 @@
+ require_relative "../base_test"
+
+module Tensorflow
+  module Data
+    class ShuffleDatasetTest < BaseTest
+      def test_simple
+        self.eager_and_graph do |context|
+          components = [Numo::NArray[1, 2, 3, 4],
+                        Numo::NArray[5, 6, 7, 8],
+                        Numo::NArray[9.0, 10.0, 11.0, 12.0]]
+          dataset = TensorSliceDataset.new(components).shuffle(100)
+          result = self.result(context, dataset)
+
+          assert_equal([[1, 5, 9.0],
+                        [2, 6, 10.0],
+                        [3, 7, 11.0],
+                        [4, 8, 12.0]], result.sort)
+        end
+      end
+    end
+  end
+end

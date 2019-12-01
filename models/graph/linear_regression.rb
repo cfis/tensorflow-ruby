ebd00e4a9995f79d89d23937da5ff4d00a68284d
Charlie Savage <cfis@savagexi.com>
2019-11-30 20:35:53 -0800

    Linear regresssion with logging.

--- models/graph/linear_regression.rb
+++ models/graph/linear_regression.rb
@@ -1,75 +1,83 @@
 $LOAD_PATH.unshift(File.expand_path('../../lib', __dir__))
 
 require "tensorflow"
-Tensorflow.disable_eager_execution
+require 'pathname'
 
-learning_rate = 0.01
-training_epochs = 1000
-display_step = 50
+# Use graph mode execution
+Tf.disable_eager_execution
 
-train_x = [3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
-           7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1,]
+# Create training data
+train_x = 10 * Numo::Float32.new([100]).rand
+train_y = 4 + 3 * train_x + Numo::Float32.new([100]).rand
 
-train_y = [1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
-           2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3,]
+# Convert to arrays
+train_x = train_x.to_a
+train_y = train_y.to_a
 
-n_samples = train_x.size
-
-x_value = Tensorflow.placeholder(:float, name: 'x_value')
-y_value = Tensorflow.placeholder(:float, name: 'y_value')
+# Placeholders
+x_value = Tf.placeholder(:float, name: 'x_value')
+y_value = Tf.placeholder(:float, name: 'y_value')
 
 # Set model weights
-weight = Tensorflow::Variable.new(rand, name: 'weight', trainable: true)
-bias = Tensorflow::Variable.new(rand, name: 'bias', trainable: true)
+weight = Tf::Variable.new(rand, name: 'weight', trainable: true)
+bias = Tf::Variable.new(rand, name: 'bias', trainable: true)
 
 # Construct a linear model
 pred = x_value * weight + bias
 
 # Mean squared error
-cost = Tensorflow::Math.reduce_sum((pred - y_value)**2.0) / (2.0 * n_samples)
+cost = Tf::Math.reduce_sum((pred - y_value)**2.0) / (2.0 * train_y.length)
 
-optimizer = Tensorflow::Train::GradientDescentOptimizer.new(learning_rate).minimize(cost)
+# Set up an optimizer
+learning_rate = 0.05
+optimizer = Tf::Train::GradientDescentOptimizer.new(learning_rate).minimize(cost)
 
-session = Tensorflow::Graph::Session.new(Tensorflow::Graph::Graph.default, Tensorflow::Graph::SessionOptions.new)
-session.run(Tensorflow.global_variables_initializer)
+# Create a new session and initialize variables
+session = Tf::Graph::Session.new(Tf::Graph::Graph.default, Tf::Graph::SessionOptions.new)
+session.run(Tf.global_variables_initializer)
 
+# Setup a variable to keep track of the epoch and get an op to increment it
+epoch_var = Tf::Variable.new(1, dtype: :int64)
+session.run(epoch_var.initializer)
+epoch_var_add_op = epoch_var.assign_add(1)
 
-writer = Tensorflow::Summary.create_file_writer('c:\temp\ruby')
-flush = writer.flush
-
-writer.step = 1
+# Enable logging to Tensorbaord - create a file writer and initialize it
+path = File.join(Dir.tmpdir, 'tensorflow-ruby')
+Pathname(path).glob('*').each(&:delete)
+writer = Tf::Summary.create_file_writer(path)
+writer.step = epoch_var
+writer_flush_op = writer.flush
 session.run(writer.initializer)
 
-op = writer.graph(Tensorflow::Graph::Graph.default)
-session.run(op)
-session.run(flush)
+# Log the graph
+session.run(writer.graph(Tf::Graph::Graph.default))
+session.run(writer_flush_op)
 
-result = session.run([cost], x_value => 5.0, y_value => 10.0)
-
+# Setup op to log cost
+write_cost_op = writer.scalar("Cost", cost)
 
 start_time = Time.now
 
-(0..training_epochs).each_with_index do |epoch, i|
+# Train the data over 250 epochs
+(0..250).each_with_index do |epoch, i|
   train_x.zip(train_y).each do |x, y|
     session.run(optimizer, {x_value => x, y_value => y})
   end
 
   current_cost = session.run(cost, {x_value => train_x, y_value => train_y})
-  writer.step = i
-  op1 = writer.scalar('cost', current_cost )
-  session.run(op1)
-  session.run(flush)
 
-  if (epoch + 1) % display_step == 0
-    current_cost = session.run(cost, {x_value => train_x, y_value => train_y})
-    current_weight = session.run(weight)
-    current_bias = session.run(bias)
+  # Log the cost
+  session.run(write_cost_op, {x_value => train_x, y_value => train_y})
+  session.run(writer_flush_op)
 
-    puts("Epoch:", "%04d" % (epoch + 1), "cost=", current_cost, "W=", current_weight, "b=", current_bias)
-  end
+  # Increment the epoch variable
+  session.run(epoch_var_add_op)
+  current_cost, current_weight, current_bias = session.run([cost, weight, bias], {x_value => train_x, y_value => train_y})
+  STDOUT << "Epoch: " << epoch << ", cost= " << current_cost << ", W=" << current_weight << ", b=" << current_bias << "\n"
 end
 
-puts "Optimization Finished!"
-training_cost = session.run(cost, {x_value => train_x, y_value => train_y})
-puts "Training cost=", training_cost, "W=", session.run(weight), "b=", session.run(bias), '\n'
-puts "time elapsed ", Time.now.to_i - start_time.to_i
+STDOUT << "\n"
+STDOUT << "Optimization Finished!" << "\n"
+STDOUT << "Time: " << (Time.now - start_time) << " seconds"
+current_cost, current_weight, current_bias = session.run([cost, weight, bias], {x_value => train_x, y_value => train_y})
+STDOUT << "Cost= " << current_cost << ", W=" << current_weight << ", b=" << current_bias << "\n"
